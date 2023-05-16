5aa172d853e5858c50c1940d463af0b50ec804c0
franzivanedep <113493553+franzivanedep@users.noreply.github.com>
2023-05-17 00:06:25 +0800

    eto gamitin niyo pokingina

---
+++ letmethink/upload/admin/config.php
@@ -0,0 +1,36 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://localhost/letmethink/upload/admin/');
+define('HTTP_CATALOG', 'http://localhost/letmethink/upload/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://localhost/letmethink/upload/admin/');
+define('HTTPS_CATALOG', 'http://localhost/letmethink/upload/');
+
+// DIR
+define('DIR_APPLICATION', 'C:/xampp/htdocs/letmethink/upload/admin/');
+define('DIR_SYSTEM', 'C:/xampp/htdocs/letmethink/upload/system/');
+define('DIR_IMAGE', 'C:/xampp/htdocs/letmethink/upload/image/');
+define('DIR_STORAGE', DIR_SYSTEM . 'storage/');
+define('DIR_CATALOG', 'C:/xampp/htdocs/letmethink/upload/catalog/');
+define('DIR_LANGUAGE', DIR_APPLICATION . 'language/');
+define('DIR_TEMPLATE', DIR_APPLICATION . 'view/template/');
+define('DIR_CONFIG', DIR_SYSTEM . 'config/');
+define('DIR_CACHE', DIR_STORAGE . 'cache/');
+define('DIR_DOWNLOAD', DIR_STORAGE . 'download/');
+define('DIR_LOGS', DIR_STORAGE . 'logs/');
+define('DIR_MODIFICATION', DIR_STORAGE . 'modification/');
+define('DIR_SESSION', DIR_STORAGE . 'session/');
+define('DIR_UPLOAD', DIR_STORAGE . 'upload/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_DATABASE', 'TIPTOOLROOMDATABASES');
+define('DB_PORT', '3307');
+define('DB_PREFIX', 'oc_');
+
+// OpenCart API
+define('OPENCART_SERVER', 'https://www.opencart.com/');
---
+++ letmethink/upload/config.php
@@ -0,0 +1,30 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://localhost/letmethink/upload/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://localhost/letmethink/upload/');
+
+// DIR
+define('DIR_APPLICATION', 'C:/xampp/htdocs/letmethink/upload/catalog/');
+define('DIR_SYSTEM', 'C:/xampp/htdocs/letmethink/upload/system/');
+define('DIR_IMAGE', 'C:/xampp/htdocs/letmethink/upload/image/');
+define('DIR_STORAGE', DIR_SYSTEM . 'storage/');
+define('DIR_LANGUAGE', DIR_APPLICATION . 'language/');
+define('DIR_TEMPLATE', DIR_APPLICATION . 'view/theme/');
+define('DIR_CONFIG', DIR_SYSTEM . 'config/');
+define('DIR_CACHE', DIR_STORAGE . 'cache/');
+define('DIR_DOWNLOAD', DIR_STORAGE . 'download/');
+define('DIR_LOGS', DIR_STORAGE . 'logs/');
+define('DIR_MODIFICATION', DIR_STORAGE . 'modification/');
+define('DIR_SESSION', DIR_STORAGE . 'session/');
+define('DIR_UPLOAD', DIR_STORAGE . 'upload/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_DATABASE', 'TIPTOOLROOMDATABASES');
+define('DB_PORT', '3307');
+define('DB_PREFIX', 'oc_');
