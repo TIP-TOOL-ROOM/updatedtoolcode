a49a74ddb0a2429808b9f3052b934e360dd55a8e
franzivanedep <113493553+franzivanedep@users.noreply.github.com>
2023-05-17 00:37:43 +0800

    databses na gamitin

--- letmethink/upload/catalog/language/en-gb/common/cart.php
+++
@@ -1,7 +0,0 @@
-<?php
-// Text
-$_['text_items']     = '%s item(s) - %s';
-$_['text_empty']     = 'Your toolbox is empty!';
-$_['text_cart']      = 'View Toolbox';
-$_['text_checkout']  = 'Checkout';
-$_['text_recurring'] = 'Payment Profile';
